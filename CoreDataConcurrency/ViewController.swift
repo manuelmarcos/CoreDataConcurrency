46264cbb3e6d05a43ad29c9e470496ee63c137d6
Manuel Marcos Regalado <manuel@ribot.co.uk>
2016-09-05 11:49:29 +0100

    Fisrst commit

---
+++ CoreDataConcurrency/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataConcurrency
+//
+//  Created by Manuel Marcos Regalado on 05/09/2016.
+//  Copyright © 2016 Manuel Marcos Regalado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
